3e60f7e75a2247a12e2cc53eebbea5b33a40b0db
Nikita Wagner <nikita.leon.wagner@gmail.com>
2023-11-23 14:49:50 +0100

    fix: change cv foldername

--- src/helper/cvLanguages.js
+++ src/helper/cvLanguages.js
@@ -1,17 +1,17 @@
 export const cvLanguages = [
 	{
 		language: "english",
-		file: "./cv/WAGNER_Nikita_CV_en.pdf",
+		file: "./cvs/WAGNER_Nikita_CV_en.pdf",
 		flag: "./flags/gb.svg",
 	},
 	{
 		language: "german",
-		file: "./cv/WAGNER_Nikita_CV_de.pdf",
+		file: "./cvs/WAGNER_Nikita_CV_de.pdf",
 		flag: "./flags/de.svg",
 	},
 	{
 		language: "french",
-		file: "./cv/WAGNER_Nikita_CV_fr.pdf",
+		file: "./cvs/WAGNER_Nikita_CV_fr.pdf",
 		flag: "./flags/fr.svg",
 	},
 ];
