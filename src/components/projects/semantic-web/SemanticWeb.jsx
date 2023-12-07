9d0958ee3246968232aca9e58185dbe28867cc03
Nikita Wagner <nikita.leon.wagner@gmail.com>
2023-12-07 21:40:15 +0100

    feat: add buttons to personalize query

--- src/components/projects/semantic-web/SemanticWeb.jsx
+++ src/components/projects/semantic-web/SemanticWeb.jsx
@@ -1,40 +1,94 @@
 import React, { useEffect, useState } from "react";
+import { Fragment } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import Tooltip from "react-simple-tooltip";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import LoadingSpinner from "../../LoadingSpinner";
 
-const geoUrl = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json";
+const filterOptions = ["Population", "Humand Developement Index"];
+const yearOptions = [
+    "1999",
+    "2000",
+    "2001",
+    "2002",
+    "2003",
+    "2004",
+    "2005",
+    "2006",
+    "2007",
+    "2008",
+    "2009",
+    "2010",
+    "2011",
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+    "2020",
+    "2021"
+];
 
 const SemanticWeb = () => {
-    const [countryData, setCountryData] = useState({
-        germany: {
-            color: "#FF0000",
-            value: "120"
-        },
-        poland: {
-            color: "#0000FF",
-            value: "99"
-        }
-    });
+    const [requestIsLoading, setRequestIsLoading] = useState(false);
+    const [requestOptions, setRequestOptions] = useState(0);
+    const [requestYear, setRequestYear] = useState(yearOptions.length - 1);
+    const [countryData, setCountryData] = useState({});
+    const makeSparqlRequest = () => {
+        // make a 5 sec delay to show the loading animation
+        setRequestIsLoading(true);
+        setTimeout(() => {
+            setRequestIsLoading(false);
+        }, 5000);
+    };
     return (
         <div className="container flex justify-center bg-white">
             <div className="container-map w-full md:w-2/4">
+                <div className="flex flex-wrap justify-end gap-2">
+                    <YearDropdown
+                        title="Option"
+                        data={filterOptions}
+                        selectedObject={requestOptions}
+                        selectObject={setRequestOptions}
+                    />
+                    <YearDropdown
+                        title="Year"
+                        data={yearOptions}
+                        selectedObject={requestYear}
+                        selectObject={setRequestYear}
+                    />
+                    {requestIsLoading ? (
+                        <LoadingSpinner />
+                    ) : (
+                        <div
+                            className="rounded-md bg-primary px-3 py-2 text-sm 
+                                    font-semibold text-white shadow-sm
+                                    hover:cursor-pointer hover:bg-secondary"
+                            onClick={() => makeSparqlRequest()}>
+                            GO
+                        </div>
+                    )}
+                </div>
                 <ComposableMap
                     width={3000}
                     height={3000}
                     projectionConfig={{ scale: 5000, center: [10, 50] }}
-                    style={{
-                        scale: "1"
-                    }}>
+                    className={`${
+                        requestIsLoading ? "animate-pulse opacity-5" : null
+                    }}`}>
                     <Geographies geography="https://unpkg.com/world-atlas@2.0.2/countries-110m.json">
                         {({ geographies }) =>
                             geographies.map((geo) => {
                                 const countryName =
                                     geo.properties.name.toLowerCase();
-                                console.log(
-                                    countryData[countryName]
-                                        ? countryData[countryName].value
-                                        : "No Content :("
-                                );
+                                // console.log(
+                                //     countryData[countryName]
+                                //         ? countryData[countryName].value
+                                //         : "No Content :("
+                                // );
                                 return (
                                     <Geography
                                         key={`${geo.rsmKey}-${geo.id}`}
@@ -56,5 +110,66 @@ const SemanticWeb = () => {
         </div>
     );
 };
+
+const YearDropdown = ({ title, data, selectedObject, selectObject }) => {
+    return (
+        <>
+            <Menu as="div" className="relative inline-block text-left">
+                <div>
+                    <Menu.Button
+                        className="inline-flex w-full justify-center gap-x-1.5 
+                    rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 
+                    ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        {data[selectedObject]}
+                        <ChevronDownIcon
+                            className="-mr-1 h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                        />
+                    </Menu.Button>
+                </div>
+
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95">
+                    <Menu.Items
+                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right 
+                    rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="max-h-[200px] overflow-auto py-1">
+                            {data.map((object, index) => {
+                                return (
+                                    <Menu.Item key={object}>
+                                        {({ active }) => (
+                                            <a
+                                                onClick={() => {
+                                                    selectObject(index);
+                                                }}
+                                                className={classNames(
+                                                    active
+                                                        ? "bg-gray-100 text-gray-900"
+                                                        : "text-gray-700",
+                                                    "block px-4 py-2 text-sm hover:cursor-pointer"
+                                                )}>
+                                                {object}
+                                            </a>
+                                        )}
+                                    </Menu.Item>
+                                );
+                            })}
+                        </div>
+                    </Menu.Items>
+                </Transition>
+            </Menu>
+        </>
+    );
+};
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
 
 export default SemanticWeb;
