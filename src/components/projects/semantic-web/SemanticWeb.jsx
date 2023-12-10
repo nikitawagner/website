13dbc6f8088d3b606846f42c1b763099d6e281f4
Nikita Wagner <nikita.leon.wagner@gmail.com>
2023-12-10 18:23:42 +0100

    feat: finalize BD

--- src/components/projects/semantic-web/SemanticWeb.jsx
+++ src/components/projects/semantic-web/SemanticWeb.jsx
@@ -5,6 +5,9 @@ import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import LoadingSpinner from "../../LoadingSpinner";
 import axios from "axios";
+import { objectIsEmpty } from "../../../helper/objectIsEmpty";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import SimpleTooltip from "../../SimpleTooltip";
 
 const filterOptions = ["Accident Rate", "Adjusted Accident Rate (HDI) "];
 const yearOptions = [
@@ -37,8 +40,10 @@ const SemanticWeb = () => {
     const [requestIsLoading, setRequestIsLoading] = useState(false);
     const [requestResult, setRequestResult] = useState("");
     const [requestOptions, setRequestOptions] = useState(0);
+    const [currentHoveredCountry, setCurrentHoveredCountry] = useState("");
     const [requestYear, setRequestYear] = useState(yearOptions.length - 1);
     const [countryData, setCountryData] = useState({});
+
     const makeSparqlRequest = async () => {
         setRequestIsLoading(true);
         let result;
@@ -74,7 +79,16 @@ const SemanticWeb = () => {
     return (
         <div className="container flex justify-center bg-white">
             <div className="container-map w-full md:w-2/4">
-                <div className="flex flex-wrap justify-end gap-2">
+                <div className="flex flex-wrap items-center justify-end gap-2">
+                    <SimpleTooltip
+                        text={
+                            requestOptions === 0
+                                ? "How many road accidents happen per 100.000 people ((Accidents / Population) * 100.000)"
+                                : "How many road accidents happen per 100.000 people adjusted by the Human Development Index (((Accidents / Population) * 100.000) / HDI)"
+                        }>
+                        <FaRegQuestionCircle className="text-gray-500 hover:text-gray-700" />
+                    </SimpleTooltip>
+
                     <YearDropdown
                         title="Option"
                         data={filterOptions}
@@ -99,39 +113,94 @@ const SemanticWeb = () => {
                         </div>
                     )}
                 </div>
-                <ComposableMap
-                    width={3000}
-                    height={3000}
-                    projectionConfig={{ scale: 5000, center: [10, 50] }}
-                    className={`${
-                        requestIsLoading ? "animate-pulse opacity-5" : null
-                    }}`}>
-                    <Geographies geography="https://unpkg.com/world-atlas@2.0.2/countries-110m.json">
-                        {({ geographies }) =>
-                            geographies.map((geo) => {
-                                const countryName =
-                                    geo.properties.name.toLowerCase();
-                                return (
-                                    <Geography
-                                        key={`${geo.rsmKey}-${geo.id}`}
-                                        geography={geo}
-                                        fill={
-                                            countryData[countryName]
-                                                ? requestOptions === 0
-                                                    ? countryData[countryName]
-                                                          .colorAccidentRatePer100k
-                                                    : countryData[countryName]
-                                                          .colorAdjustedAccidentRate
-                                                : "#EAEAEC"
-                                        }
-                                        stroke="#000"
-                                        strokeWidth={3}
-                                    />
-                                );
-                            })
-                        }
-                    </Geographies>
-                </ComposableMap>
+                <div className="flex gap-1">
+                    <ComposableMap
+                        width={3000}
+                        height={3000}
+                        projectionConfig={{ scale: 5000, center: [10, 50] }}
+                        className={`${
+                            requestIsLoading ? "animate-pulse opacity-5" : null
+                        }}`}>
+                        <Geographies geography="https://unpkg.com/world-atlas@2.0.2/countries-110m.json">
+                            {({ geographies }) =>
+                                geographies.map((geo) => {
+                                    const countryName =
+                                        geo.properties.name.toLowerCase();
+                                    return (
+                                        <Geography
+                                            key={`${geo.rsmKey}-${geo.id}`}
+                                            geography={geo}
+                                            fill={
+                                                countryData[countryName]
+                                                    ? requestOptions === 0
+                                                        ? countryData[
+                                                              countryName
+                                                          ]
+                                                              .colorAccidentRatePer100k
+                                                        : countryData[
+                                                              countryName
+                                                          ]
+                                                              .colorAdjustedAccidentRate
+                                                    : "#EAEAEC"
+                                            }
+                                            stroke="#000"
+                                            strokeWidth={3}
+                                            onMouseEnter={() => {
+                                                setCurrentHoveredCountry(
+                                                    countryName
+                                                );
+                                            }}
+                                            onMouseLeave={() => {
+                                                setCurrentHoveredCountry("");
+                                            }}
+                                        />
+                                    );
+                                })
+                            }
+                        </Geographies>
+                    </ComposableMap>
+                    {!objectIsEmpty(countryData) ? (
+                        <Legend
+                            countryData={countryData}
+                            option={requestOptions}
+                        />
+                    ) : null}
+                </div>
+                <div
+                    className={`mt-1 inline-flex w-full justify-center 
+                    gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold 
+                    text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${
+                        currentHoveredCountry !== "" &&
+                        countryData[currentHoveredCountry]?.accidentRatePer100k
+                            ? "shadow shadow-primary"
+                            : null
+                    }`}>{`${
+                    currentHoveredCountry
+                        ? `${currentHoveredCountry.toLocaleUpperCase()}: `
+                        : ""
+                } ${
+                    !objectIsEmpty(countryData)
+                        ? currentHoveredCountry !== ""
+                            ? requestOptions === 0
+                                ? `${
+                                      Math.floor(
+                                          countryData[currentHoveredCountry]
+                                              ?.accidentRatePer100k * 100
+                                      ) / 100
+                                  } accidents per 100k`
+                                : `Adjusted Accident Rate ${
+                                      Math.floor(
+                                          countryData[currentHoveredCountry]
+                                              ?.adjustedAccidentRate * 100
+                                      ) / 100
+                                  }`
+                            : `Hover to see ${
+                                  requestOptions === 0
+                                      ? "Accident Rate"
+                                      : "Adjusted Accident Rate (HDI)"
+                              }`
+                        : "Press go to fetch data"
+                }`}</div>
             </div>
         </div>
     );
@@ -191,6 +260,123 @@ const YearDropdown = ({ title, data, selectedObject, selectObject }) => {
                 </Transition>
             </Menu>
         </>
+    );
+};
+
+export const Legend = ({ countryData, option }) => {
+    const [lowerPart, setLowerPart] = useState({});
+    const [upperPart, setUpperPart] = useState({});
+    const [lowerColor, setLowerColor] = useState("");
+    const [upperColor, setUpperColor] = useState("");
+
+    const findExtremesInAccidentRates = (data) => {
+        let maxRateCountry = null;
+        let minRateCountry = null;
+        let maxAdjustedRateCountry = null;
+        let minAdjustedRateCountry = null;
+        let maxAdjustedRate = -Infinity;
+        let minAdjustedRate = Infinity;
+        let maxRate = -Infinity;
+        let minRate = Infinity;
+
+        for (const country in data) {
+            const rate = data[country].accidentRatePer100k;
+            const adjustedRate = data[country].adjustedAccidentRate;
+            if (rate > maxRate) {
+                maxRate = rate;
+                maxRateCountry = country;
+            }
+            if (rate < minRate) {
+                minRate = rate;
+                minRateCountry = country;
+            }
+            if (adjustedRate > maxAdjustedRate) {
+                maxAdjustedRate = adjustedRate;
+                maxAdjustedRateCountry = country;
+            }
+            if (adjustedRate < minAdjustedRate) {
+                minAdjustedRate = adjustedRate;
+                minAdjustedRateCountry = country;
+            }
+        }
+
+        return {
+            maxRateCountry,
+            minRateCountry,
+            maxAdjustedRateCountry,
+            minAdjustedRateCountry
+        };
+    };
+
+    useEffect(() => {
+        const {
+            maxRateCountry,
+            minRateCountry,
+            maxAdjustedRateCountry,
+            minAdjustedRateCountry
+        } = findExtremesInAccidentRates(countryData);
+        if (!objectIsEmpty(countryData)) {
+            if (option === 0) {
+                const lowerColor =
+                    countryData[minRateCountry].colorAccidentRatePer100k;
+                const upperColor =
+                    countryData[maxRateCountry].colorAccidentRatePer100k;
+                const lowerValue =
+                    countryData[minRateCountry].accidentRatePer100k;
+                const upperValue =
+                    countryData[maxRateCountry].accidentRatePer100k;
+                setLowerPart({
+                    color: lowerColor,
+                    value: (Math.round(lowerValue * 100) / 100).toFixed(2)
+                });
+                setLowerColor(lowerColor.toLowerCase());
+                setUpperPart({
+                    color: upperColor,
+                    value: (Math.round(upperValue * 100) / 100).toFixed(2)
+                });
+                setUpperColor(upperColor.toLowerCase());
+            } else {
+                const lowerColor =
+                    countryData[minAdjustedRateCountry]
+                        .colorAdjustedAccidentRate;
+                const upperColor =
+                    countryData[maxRateCountry].colorAdjustedAccidentRate;
+                const lowerValue =
+                    countryData[minAdjustedRateCountry].adjustedAccidentRate;
+                const upperValue =
+                    countryData[maxAdjustedRateCountry].adjustedAccidentRate;
+                setLowerPart({
+                    color: lowerColor,
+                    value: (Math.round(lowerValue * 100) / 100).toFixed(2)
+                });
+                setLowerColor(lowerColor.toLowerCase());
+                setUpperPart({
+                    color: upperColor,
+                    value: (Math.round(upperValue * 100) / 100).toFixed(2)
+                });
+                setUpperColor(upperColor.toLowerCase());
+            }
+        }
+    }, [countryData, option]);
+
+    return (
+        <div className="flex gap-1 py-2">
+            <div
+                style={{
+                    background: `linear-gradient(to top, ${lowerColor}, ${upperColor})`,
+                    height: "100%",
+                    width: "10px",
+                    borderRadius: "10px"
+                }}
+            />
+            <div className="flex flex-col-reverse justify-between text-xs font-medium">
+                <div>{lowerPart?.value}</div>
+                <div>
+                    {(Number(lowerPart?.value) + Number(upperPart?.value)) / 2}
+                </div>
+                <div>{upperPart?.value}</div>
+            </div>
+        </div>
     );
 };
 
